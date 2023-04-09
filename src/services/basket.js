5a73bb85d31c850c3e077631301a29b6afae7117
Thiago Villa <thiago+gh@thiagovilla.com>
2023-04-09 14:09:05 -0300

    fix: order total formula and update on edit qty

--- src/services/basket.js
+++ src/services/basket.js
@@ -9,10 +9,7 @@ export async function getBasket() {
 
 export async function addToBasket(products) {
   basket.products.push(...products);
-  basket.total += products.reduce(
-    (total, product) => total + product.price * product.quantity,
-    0
-  );
+  updateOrderTotal();
   return Promise.resolve(basket);
 }
 
@@ -20,6 +17,7 @@ export async function updateBasketQuantities(prodQtyMap) {
   basket.products.forEach(
     product => (product.quantity = prodQtyMap[product.id])
   );
+  updateOrderTotal();
   return Promise.resolve(basket);
 }
 
@@ -27,4 +25,11 @@ export async function clearBasket() {
   basket.products = [];
   basket.total = 0;
   return Promise.resolve(basket);
+}
+
+function updateOrderTotal() {
+  basket.total = basket.products.reduce(
+    (total, product) => product.price * product.quantity,
+    0
+  );
 }
