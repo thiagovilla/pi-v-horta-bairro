b9a572a5598f2d6f1ecc27e9e1d3267116174d19
Thiago Villa <thiago+gh@thiagovilla.com>
2023-04-09 09:04:25 -0300

    feat: add fresh product list w/ dummy data

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -9,8 +9,21 @@ function Store() {
   return (
     <div>
       <h1>{store.name}</h1>
+      <FreshToday products={store.freshToday} />
     </div>
   );
+}
+
+function FreshToday(props) {
+  if (props.products.length < 1) return <div>Sem produtos frescos hoje.</div>;
+
+  const listItems = props.products.map(product => (
+    <li>
+      {product.name} - {product.price}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
 }
 
 export default Store;
--- src/services/stores.js
+++ src/services/stores.js
@@ -2,13 +2,15 @@ const stores = [
   {
     slug: "my-store",
     name: "My Store",
+    freshToday: [],
   },
   {
     slug: "horta-123",
     name: "Horta 123",
+    freshToday: [{ name: "Alface", price: 3.5 }],
   },
 ];
 
 export function getStoreBySlug(slug) {
-  return stores.find(s => slug === s.slug);
+  return Promise.resolve(stores.find(s => slug === s.slug));
 }
